459814c1a4029e02c49d78c325a724c14bb336a4
Moosecoop <cooper.makhijani@icloud.com>
2017-08-25 16:47:41 -0700

    This is possibly the smallest CPP interactive application

--- SmallestInteractiveCPPApp/SmallestInteractiveCPPApp.cpp
+++ SmallestInteractiveCPPApp/SmallestInteractiveCPPApp.cpp
@@ -1,11 +1,4 @@
-// SmallestInteractiveCPPApp.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
-
-int main()
-{
-    return 0;
-}
-
+#include <iostream>
+#include <windows.h>
+using namespace std; int main() { SetConsoleTitle(TEXT("Number In Common | Copyright (c) sudomoose 2017 - all rights reserved | moosehub.xyz/smp")); int a; cout << "Please enter a number \n"; cin >> a; cout << "Nice! " << a << " is my favorite number!\n"; cout << "That's so neat! We have something in common!\n"; return 0; }
